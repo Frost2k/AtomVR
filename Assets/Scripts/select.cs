1d6536e944a217c959fdb5cb3d1e57517aff5172
Thomas Scott <Pheonix2k@gmail.com>
2018-03-20 09:21:10 -0500

    Added select script to atom objects.

---
+++ Assets/Scripts/select.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class select : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnMouseDown(){
+		// this object was clicked - do something
+		Destroy (this.gameObject);
+	}   
+}
